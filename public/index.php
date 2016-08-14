e750ab8bbd6299c52559e0e293b9284487c8cc6b
sancaweb <sanca.snake@gmail.com>
2016-08-15 02:07:54 +0700

    penggantian panjul

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ define('APP', dirname(__FILE__) . '/../app/');
 define('INDEX_FILE', basename(__FILE__));
 
 // And the GEAR constant is where panada folder located.
-define('GEAR', dirname(__DIR__).'/panada/');
+define('GEAR', dirname(__DIR__).'/panjul/');
 
 define('TMP','tmp/');
 define('TEMPLATE','v1/');
